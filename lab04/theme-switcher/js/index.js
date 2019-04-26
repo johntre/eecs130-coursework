5ed67ee8d84361ebcfb1fc8fb8dacd92173bc918
John Drake <johndrake@dhcp-10-105-85-85.wireless.northwestern.private>
2019-04-26 14:21:52 -0500

    Adding my lab04 to the repo

---
+++ lab04/theme-switcher/js/index.js
@@ -0,0 +1,18 @@
+const defaultTheme = () => {
+   document.querySelector('body').className= '';
+};
+
+const oceanTheme = () => {
+  document.querySelector('body').className= 'ocean';
+
+};
+
+const desertTheme = () => {
+  document.querySelector('body').className= 'desert';
+
+};
+
+
+document.querySelector('#default').onclick = defaultTheme;
+document.querySelector("#ocean").onclick = oceanTheme;
+document.querySelector("#desert").onclick = desertTheme;
