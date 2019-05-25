32b24b3b0b417566d21e943709e336ca8f367faf
John Drake <johndrake@dhcp-10-105-85-85.wireless.northwestern.private>
2019-05-24 19:10:26 -0500

    Adding my lab08 to the repo

---
+++ lab08/get_delete_demo/photos.js
@@ -0,0 +1,47 @@
+const myAPIEndpoint = 'https://johntre.herokuapp.com/photos';
+
+const getData = () => {
+    fetch(myAPIEndpoint)
+        .then(response => response.json())
+        .then(data => {
+            // 3. print the results to the screen
+            const container = document.querySelector('.results');
+            container.innerHTML = '';
+            for (item of data) {
+                container.innerHTML += generateEntryHTML(item)
+            }
+            attachDeleteEventHandlers();
+        });
+};
+
+const generateEntryHTML = (photo) => {
+    return `
+        <div class="card">
+          <img src= "${photo.url}"/>
+            <p>${photo.caption}</p>
+            <p>
+                <a data-id="${photo.id}" href="#" class="delete">delete</a>
+            </p>
+        </div>`;
+};
+
+const attachDeleteEventHandlers = () => {
+    for (link of document.querySelectorAll('.delete')) {
+        link.onclick = deleteEntry;
+    }
+};
+
+const deleteEntry = (ev) => {
+    const id = ev.target.getAttribute('data-id');
+    fetch(myAPIEndpoint + '/' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        getData();
+    });
+    return ev.preventDefault();
+}
+
+getData();
---
+++ lab08/get_delete_demo/posts.js
@@ -0,0 +1,48 @@
+const myAPIEndpoint = 'https://johntre.herokuapp.com/posts';
+
+const getData = () => {
+    fetch(myAPIEndpoint)
+        .then(response => response.json())
+        .then(data => {
+            // 3. print the results to the screen
+            const container = document.querySelector('.results');
+            container.innerHTML = '';
+            for (item of data) {
+                container.innerHTML += generateEntryHTML(item);
+            }
+            attachDeleteEventHandlers();
+        });
+};
+
+const generateEntryHTML = (post) => {
+    return `
+    <div class="card">
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <p>
+            <a data-id="${post.id}" href="#" class="delete">delete</a>
+        </p>
+    </div>
+    `;
+};
+
+const attachDeleteEventHandlers = () => {
+    for (link of document.querySelectorAll('.delete')) {
+        link.onclick = deleteEntry;
+    }
+};
+
+const deleteEntry = (ev) => {
+    const id = ev.target.getAttribute('data-id');
+    fetch(myAPIEndpoint + '/' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        getData();
+    });
+    return ev.preventDefault();
+}
+
+getData();
